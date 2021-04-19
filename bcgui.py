325d803bc092dd08f8797d8c635274524682f1a6
Bryan Wolff <bryan.wolff@hotmail.com>
2021-04-19 15:55:23 -0300

    Update bcgui.py

--- bcgui.py
+++ bcgui.py
@@ -5,8 +5,10 @@ from dashLayout import app
 #OBS: para evitar o erro 'lazy loading' execute esse arquivo com o seguinte código no terminal: waitress-serve interface:app.server
 
 if __name__ == '__main__':
-	if(len(sys.argv) >= 2):
+	if(len(sys.argv) == 2):
 		app.run_server(debug=True, use_reloader=True, host=sys.argv[1],port=8050)
+	elif (len(sys.argv) == 3):
+		app.run_server(debug=True, use_reloader=True, host=sys.argv[1],port=sys.argv[2])
 	else:
 		app.run_server(debug=True, use_reloader=True, host='127.0.0.1',port=8050)
 	#debug=True significa que o Dash atualizará automaticamente o navegador quando você fizer uma alteração no código.
