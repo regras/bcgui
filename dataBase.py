e434499ce416816f352ff956bc9af408cf4513c6
Bryan Wolff <bryan.wolff@hotmail.com>
2021-06-15 19:30:55 -0300

    Update dataBase.py

--- dataBase.py
+++ dataBase.py
@@ -37,6 +37,7 @@ def blockchain_list(rangeID):
 			blocks_localChains[i].append(colors['node-unstable'])
 		i = i + 1
 
+    #Blocos não selecionados
 	#Cria uma lista - cada termo é um bloco da log_block - cada bloco possui na ordem: id, hash, prev_hash, arrive_time, round, proof_hash
 	cursor.execute('SELECT id, hash, prev_hash, arrive_time, round, proof_hash, subuser FROM log_block t1 WHERE t1.id BETWEEN (SELECT {} FROM log_block) AND (SELECT {} FROM log_block) and EXISTS (SELECT * FROM log_block t2 WHERE t2.round <= t1.round and t1.id == t2.id and t2.arrive_time < t1.arrive_time and t2.proof_hash < t1.proof_hash) and NOT EXISTS (SELECT * FROM localChains t3 WHERE t3.hash == t1.hash)'.format(rangeID[0],rangeID[1]))
 	b = cursor.fetchall()
@@ -55,26 +56,44 @@ def blockchain_list(rangeID):
 		blocks_log_blocks[i].append(colors['node-log'])
 		i = i + 1
     
-	#Cria uma lista - cada termo é um bloco revertido da log_block - cada bloco possui na ordem: id, hash, prev_hash, arrive_time, round, proof_hash, subuser
+	#Cria uma lista - cada termo é um bloco selecionado uma vez, q pode ser obtido na log_block - cada bloco possui na ordem: id, hash, prev_hash, arrive_time, round, proof_hash, subuser
 	cursor.execute('SELECT id, hash, prev_hash, arrive_time, round, proof_hash, subuser FROM log_block t1 WHERE t1.id BETWEEN (SELECT {} FROM log_block) AND (SELECT {} FROM log_block) and NOT EXISTS (SELECT * FROM log_block t2 WHERE t2.round <= t1.round and t1.id == t2.id and t2.arrive_time < t1.arrive_time and t2.proof_hash < t1.proof_hash) and NOT EXISTS (SELECT * FROM localChains t3 WHERE t3.hash == t1.hash)'.format(rangeID[0],rangeID[1]))
 	c = cursor.fetchall()
-	reversed_blocks = []
+	blocks_SelectedOnce = []
 	i=0
 	for z in c:
-		reversed_blocks.append([])
-		reversed_blocks[i].append(z[0]) #id
-		reversed_blocks[i].append(z[1]) #hash
-		reversed_blocks[i].append(z[2]) #prev_hash
-		reversed_blocks[i].append(z[3]) #arrive_time (UTC)
-		reversed_blocks[i].append(z[4]) #round
-		reversed_blocks[i].append(z[5]) #proof_hash
-		reversed_blocks[i].append(z[6]) #subuser
-		reversed_blocks[i].append(datetime.datetime.utcfromtimestamp(float(z[3]))) #arrive_time (local datetime)
-		reversed_blocks[i].append(colors['node-selected-once'])
+		blocks_SelectedOnce.append([])
+		blocks_SelectedOnce[i].append(z[0]) #id
+		blocks_SelectedOnce[i].append(z[1]) #hash
+		blocks_SelectedOnce[i].append(z[2]) #prev_hash
+		blocks_SelectedOnce[i].append(z[3]) #arrive_time (UTC)
+		blocks_SelectedOnce[i].append(z[4]) #round
+		blocks_SelectedOnce[i].append(z[5]) #proof_hash
+		blocks_SelectedOnce[i].append(z[6]) #subuser
+		blocks_SelectedOnce[i].append(datetime.datetime.utcfromtimestamp(float(z[3]))) #arrive_time (local datetime)
+		blocks_SelectedOnce[i].append(colors['node-selected-once'])
 		i = i + 1
 
+	"""#Cria uma lista - cada termo é um bloco revertido da blockchain - cada bloco possui na ordem: id, hash, prev_hash, arrive_time, round, proof_hash, subuser
+	cursor.execute('SELECT id, hash, prev_hash, arrive_time, round, proof_hash, subuser FROM log_block t1 WHERE t1.id BETWEEN (SELECT {} FROM log_block) AND (SELECT {} FROM log_block) and NOT EXISTS (SELECT * FROM log_block t2 WHERE t2.round <= t1.round and t1.id == t2.id and t2.arrive_time < t1.arrive_time and t2.proof_hash < t1.proof_hash) and NOT EXISTS (SELECT * FROM localChains t3 WHERE t3.hash == t1.hash)'.format(rangeID[0],rangeID[1]))
+	d = cursor.fetchall()
+	reversed_blocks = []
+	i=0
+	for w in d:
+		reversed_blocks.append([])
+		reversed_blocks[i].append(w[0]) #id
+		reversed_blocks[i].append(w[1]) #hash
+		reversed_blocks[i].append(w[2]) #prev_hash
+		reversed_blocks[i].append(w[3]) #arrive_time (UTC)
+		reversed_blocks[i].append(w[4]) #round
+		reversed_blocks[i].append(w[5]) #proof_hash
+		reversed_blocks[i].append(w[6]) #subuser
+		reversed_blocks[i].append(datetime.datetime.utcfromtimestamp(float(w[3]))) #arrive_time (local datetime)
+		reversed_blocks[i].append(colors['node-rev'])
+		i = i + 1"""
+
 	db.close()
-	return blocks_localChains, blocks_log_blocks, reversed_blocks
+	return blocks_localChains, blocks_log_blocks, blocks_SelectedOnce #, reversed_blocks
 
 
 #função para buscar algumas estatisticas da cadeia (blocos produzidos, revertidos, etc)
